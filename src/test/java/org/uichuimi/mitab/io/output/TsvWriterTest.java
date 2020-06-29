f5c8b3ad34757d0bffb640499e925e20976e4577
Pascual Lorente <pasculorente@gmail.com>
2020-06-29 20:59:01 +0100

    Removed use of temporary file, using in memory byte array instead

--- src/test/java/org/uichuimi/mitab/io/output/TsvWriterTest.java
+++ src/test/java/org/uichuimi/mitab/io/output/TsvWriterTest.java
@@ -5,8 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.uichuimi.mitab.io.input.InteractionReader;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
@@ -16,17 +15,14 @@ public class TsvWriterTest {
 	@Test
 	public void write() throws Exception {
 		final InputStream resource = getClass().getResourceAsStream("/input/sample-interactions.mitab27");
-		final File tempFile = File.createTempFile("tsv", ".tsv");
+		final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		try (final InteractionReader reader = new InteractionReader(resource);
-		     final TsvWriter writer = new TsvWriter(tempFile)) {
+		     final TsvWriter writer = new TsvWriter(outputStream)) {
 			reader.forEach(writer);
 		}
-
 		try {
 			final String expected = IOUtils.toString(getClass().getResourceAsStream("/output/expected_output.tsv"), Charset.defaultCharset());
-			final String actual = IOUtils.toString(new FileInputStream(tempFile), Charset.defaultCharset());
-			for (int i = 0; i < expected.length(); i++)
-				Assert.assertEquals("" + i + " " + expected.substring(Math.max(0, i - 10), i), String.valueOf(expected.charAt(i)), String.valueOf(actual.charAt(i)));
+			final String actual = outputStream.toString();
 			Assert.assertEquals(expected, actual);
 		} catch (IOException e) {
 			e.printStackTrace();
